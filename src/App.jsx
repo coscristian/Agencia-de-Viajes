e38fc3a18656eb9d3590beb3a2f2f531d717feca
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-02-22 13:38:11 +0100

    Fix nav-bar and main title

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div className="App">
       <Router>
         <Routes>
-          <Route path="/admin" element={<Index />} /> 
+          <Route path="/" element={<Index />} /> 
           <Route path="/admin_sucursal" element={<SucursalAdmin />}/>
         </Routes>
       </Router>
--- src/pages/index.jsx
+++ src/pages/index.jsx
@@ -1,19 +1,23 @@
 import airplane from 'media/airplane.png';
 import adminIcon from 'media/admin_icon.webp';
+
 function Index() {
     return (
         <div>
             <header>
                 <ul>
                     <li><img class="logo" src={airplane} alt="Logo ViajaYa" /></li>
-                    <li><a href="#">Sucursales</a></li>
-                    <li><a href="#">Hoteles</a></li>
-                    <li><a href="#">Administradores</a></li>
-                    <li><a href="#">Vendedores</a></li>
-                    <li><a href="#">Clientes</a></li>
+                    <h2 class="title">ViajaYa</h2>
                     <li><img class="admin-icon" src={adminIcon} alt="Logo Admin" /></li>
                 </ul>
             </header>
+            <ul>
+                <li><a href="#">Sucursales</a></li>
+                <li><a href="#">Hoteles</a></li>
+                <li><a href="#">Administradores</a></li>
+                <li><a href="#">Vendedores</a></li>
+                <li><a href="#">Clientes</a></li>
+            </ul>
         </div>
     );
 }
