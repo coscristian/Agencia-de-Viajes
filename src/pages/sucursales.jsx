e92faaa0182f4d84e5b68f6303aaa8fc326df825
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-03-06 19:47:01 -0500

    Add sucursales button to CRUD operation

--- src/pages/sucursales.jsx
+++ src/pages/sucursales.jsx
@@ -10,8 +10,14 @@ function Sucursales() {
                 <li className='py-2 pl-6 pr-14 hover:bg-blue-900 rounded-3xl'>Filter by</li>
                 <li className='py-2 pl-6 pr-14 hover:bg-blue-900 rounded-3xl'>Order By</li>
             </Sidebar>
-            <div className="flex flex-col my-5 w-full justify-center items-center">
-                <h2 className="text-2xl text-gray-800 font-sans my-5 font-bold">Sucursales Disponibles</h2>
+            <div className="flex flex-col my-5 w-full">
+                <div className="flex justify-between mx-5">
+                    <h2 className="text-2xl text-gray-800 font-sans my-5 font-bold">Sucursales Disponibles</h2>
+                    <button type="button" class="px-4 focus:outline-none text-white bg-red-700 hover:bg-red-800 focus:ring-4 focus:ring-green-300 font-bold  rounded-lg text-sm dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-900">
+                        Add
+                    </button>
+                </div>
+                
                 <div className="grid grid-cols-3 gap-16 ">
                     <SucursalCard titulo="Cali" img={cali} etiqueta1="Feria" etiqueta2="Valle"/>
                     <SucursalCard titulo="Pereira" img={pereira} etiqueta1="Risaralda" etiqueta2="Cafetero"/>
