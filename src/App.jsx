5ca78e22a45603337550207122c836d8222978c5
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-03-11 02:19:02 -0500

    Route added to personal profile

--- src/App.jsx
+++ src/App.jsx
@@ -7,9 +7,10 @@ import Hotels from 'pages/hotels';
 import Admins from 'pages/admins';
 import Sellers from 'pages/sellers';
 import Customers from 'pages/customers';
-import Profile from 'layouts/Profile'
+import Profile from 'pages/profile';
 import {
   BrowserRouter as Router,
+  Outlet,
   Route,
   Routes
 } from "react-router-dom";
@@ -20,16 +21,20 @@ function App() {
     <div className="App">
       <Router>
           <Routes>
+
             <Route path="/" element={<Layout />}>
                 <Route path='/' element={<Index />} />
                 <Route path="/sucursales" element={<Sucursales />}/>
                 <Route path="/hotels" element={<Hotels />}/>
-                <Route path="/admins" element={<Admins />}/>
+                <Route path="admins" element={<Outlet />} > 
+                  <Route index element={<Admins/>}/> 
+                  <Route path="profile" element={<Profile/>}/> 
+                </Route>
                 <Route path="/sellers" element={<Sellers />}/>
                 <Route path="/customers" element={<Customers />}/>
-                <Route path="/profile" element={<Profile/>}/>
+                
             </Route> 
-            
+
             <Route path="/admin_sucursal" element={<SucursalAdmin />}/>
             
           </Routes>        
--- src/pages/admins.jsx
+++ src/pages/admins.jsx
@@ -5,7 +5,10 @@ import admin3 from 'media/admin3.jpeg';
 import admin4 from 'media/admin4.jpg';
 import SucursalCard from 'components/SucursalCard';
 import Sidebar from 'components/Sidebar';
-
+import Profile from 'pages/profile';
+import {
+  Link, Outlet
+} from "react-router-dom";
 const Admins = () => {
   return (
     <div className='flex flex-row bg-gray-100'>
@@ -16,15 +19,34 @@ const Admins = () => {
         <div className="flex flex-col my-5 w-full justify-center items-center">
             <h2 className="text-2xl my-5 font-sans font-bold">Available Admins</h2>
             <div className="grid grid-cols-3 gap-2">
-                <SucursalCard titulo="Juan Carlos Ramirez" img={admin2} etiqueta1="Pereira" etiqueta2="Risaralda"/>
-                <SucursalCard titulo="Andrea Gallardo" img={admin1} etiqueta1="Medellín" etiqueta2="Antioquía"/>
-                <SucursalCard titulo="Camilo Perez" img={admin4} etiqueta1="Bogotá" etiqueta2="Cundinamarca"/>
-                <SucursalCard titulo="Camila Montoya" img={admin3} etiqueta1="Villavicencio" etiqueta2="Meta"/>
-                <SucursalCard titulo="Camila Montoya" img={admin3} etiqueta1="Villavicencio" etiqueta2="Meta"/>
-                <SucursalCard titulo="Camila Montoya" img={admin3} etiqueta1="Villavicencio" etiqueta2="Meta"/>
-                <SucursalCard titulo="Camila Montoya" img={admin3} etiqueta1="Villavicencio" etiqueta2="Meta"/>
+                <Link to="profile">
+                  <SucursalCard titulo="Juan Carlos Ramirez" img={admin2} etiqueta1="Pereira" etiqueta2="Risaralda"/>
+                </Link>
+                <Link to="profile">
+                  <SucursalCard titulo="Andrea Gallardo" img={admin1} etiqueta1="Medellín" etiqueta2="Antioquía"/>
+                </Link>
+
+                <Link to="profile">
+                  <SucursalCard titulo="Camilo Perez" img={admin4} etiqueta1="Bogotá" etiqueta2="Cundinamarca"/>
+                </Link>
+
+                <Link to="profile">
+                  <SucursalCard titulo="Camila Montoya" img={admin3} etiqueta1="Villavicencio" etiqueta2="Meta"/>
+                </Link>
+
+                <Link to="profile">
+                  <SucursalCard titulo="Camila Montoya" img={admin3} etiqueta1="Villavicencio" etiqueta2="Meta"/>
+                </Link>
+
+                <Link to="profile">
+                  <SucursalCard titulo="Camila Montoya" img={admin3} etiqueta1="Villavicencio" etiqueta2="Meta"/>
+                </Link>
+
+                <Link to="profile">
+                  <SucursalCard titulo="Camila Montoya" img={admin3} etiqueta1="Villavicencio" etiqueta2="Meta"/>
+                </Link>
             </div>
-        </div>            
+        </div>        
     </div>
   );
 }
