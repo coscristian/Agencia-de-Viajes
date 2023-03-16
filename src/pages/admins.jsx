32ffafb1e8679d80e2f8bd12d37c5f080baa736e
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-03-16 13:38:31 -0500

    Fix bug when creating a new admin

--- src/pages/admins.jsx
+++ src/pages/admins.jsx
@@ -152,7 +152,7 @@ const FormCreateAdmin = ({ adminsList, setShowTable, setAdmins, editAdminInfo, d
             class="block py-2.5 px-0 w-full text-sm text-gray-900 bg-transparent border-0 border-b-2 border-gray-300 appearance-none dark:text-black dark:border-gray-600 dark:focus:border-blue-500 focus:outline-none focus:ring-0 focus:border-blue-600 peer"
             placeholder=" "
             required
-            value={editAdminInfo ? defaultIdAdmin: ""}
+            defaultValue={editAdminInfo ? defaultIdAdmin: ""}
             disabled={editAdminInfo ? true: false}
           />
           <label
