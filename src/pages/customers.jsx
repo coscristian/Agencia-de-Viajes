577743fca57a91c7fe85fad0b736dcc9e5d3ea40
Crristian Quesada Cossio <crisquesadaco@gmail.com>
2023-04-11 23:00:37 -0500

    Customers table and form added

--- src/pages/customers.jsx
+++ src/pages/customers.jsx
@@ -1,28 +1,154 @@
-import React from 'react';
-import admin1 from 'media/admin1.png';
-import admin2 from 'media/admin2.jpg';
-import admin3 from 'media/admin3.jpeg';
-import admin4 from 'media/admin4.jpg';
-import SucursalCard from 'components/SucursalCard';
-import Sidebar from 'components/Sidebar';
+import Sidebar from "components/Sidebar";
+import SidebarOption from "components/SidebarOption";
+import InformationLayout from "layouts/private_layout/InformationLayout";
+import { useEffect, useState } from "react";
+import EntityForm from "components/EntityForm";
+import FormGridHeader from "components/form/FormGridHeader";
+import FormInput from "components/form/FormInput";
+import Table from "components/table/Table";
+import TableBody from "components/table/TableBody";
+import TableHeader from "components/table/TableHeader";
+import TableRow from "components/table/TableRow";
+import TableHeaderColumn from "components/table/TableHeaderColumn";
+
+const customersBackend = [
+  {
+    dni: "1087489",
+    name: "Camila",
+    lastName: "Montoya",
+    sucursal: "Medellín",
+    address: "Pinares",
+    phone: "320456987",
+    email: "camila_montoya@gmail.com",
+  },
+  {
+    dni: "1189635",
+    name: "Andrea",
+    lastName: "Gallardo",
+    sucursal: "Pereira",
+    address: "Pinares",
+    phone: "320456987",
+    email: "andrea_gallardo@gmail.com",
+  },
+  {
+    dni: "4785212",
+    name: "Camilo",
+    lastName: "Perez",
+    sucursal: "Bogotá",
+    address: "Pinares",
+    phone: "320456987",
+    email: "camilo_perez@gmail.com",
+  },
+];
 
 const Customers = () => {
+  const [showTable, setShowTable] = useState(true);
+  const [customers, setCustomers] = useState([]);
+
+  useEffect(() => {
+    setCustomers(customersBackend);
+  }, []);
+
   return (
-    <div className='flex flex-row bg-gray-100'>
-        <Sidebar>
-            <li className='py-2 pl-6 pr-14 hover:bg-blue-900 rounded-3xl'>Filter by</li>
-            <li className='py-2 pl-6 pr-14 hover:bg-blue-900 rounded-3xl'>Order by</li>
-        </Sidebar>
-        <div className="flex flex-col my-5 w-full justify-center items-center">
-            <h2 className="text-2xl font-sans font-bold">Available Customers</h2>
-            <div className="grid grid-cols-3 gap-2">
-                <SucursalCard titulo="Camilo Perez" img={admin4} etiqueta1="Bogotá" etiqueta2="Cundinamarca"/>
-                <SucursalCard titulo="Camila Montoya" img={admin3} etiqueta1="Villavicencio" etiqueta2="Meta"/>
-                <SucursalCard titulo="Andrea Gallardo" img={admin1} etiqueta1="Medellín" etiqueta2="Antioquía"/>
-                <SucursalCard titulo="Juan Carlos Ramirez" img={admin2} etiqueta1="Pereira" etiqueta2="Risaralda"/>
-            </div>
-        </div>            
+    <div className="flex flex-row bg-gray-100">
+      <Sidebar>
+        <SidebarOption optionText="Filter by" />
+        <SidebarOption optionText="Order By" />
+        <SidebarOption optionText="Etc" />
+      </Sidebar>
+      <InformationLayout
+        tableTitle="Available Customers"
+        buttonTableText="Create Customer"
+        formTitle="Creating Customer"
+        buttonFormText="Show All Customers"
+        showTable={showTable}
+        setShowTable={setShowTable}
+        table={<CustomersTable customers={customers} setShowTable={setShowTable} />}
+        form={
+          <EntityForm
+            entities={customers}
+            setEntities={setCustomers}
+            setShowTable={setShowTable}
+          >
+            <FormGridHeader>
+              <FormInput
+                type="text"
+                name="dni"
+                placeholder=""
+                required={true}
+                editingInfo={false}
+                labelText="DNI"
+              />
+              <FormInput
+                type="text"
+                name="name"
+                placeholder=""
+                required={true}
+                editingInfo={false}
+                labelText="Name"
+              />
+              <FormInput
+                type="text"
+                name="lastname"
+                placeholder=""
+                required={true}
+                editingInfo={false}
+                labelText="Last Name"
+              />
+            </FormGridHeader>
+
+            <FormInput
+              type="text"
+              name="address"
+              placeholder=""
+              required={true}
+              editingInfo={false}
+              labelText="Address"
+            />
+            <FormInput
+              type="email"
+              name="email"
+              placeholder=""
+              required={true}
+              editingInfo={false}
+              labelText="Email"
+            />
+            <FormInput
+              type="tel"
+              name="phone"
+              placeholder=""
+              required={true}
+              editingInfo={false}
+              labelText="Phone"
+            />
+          </EntityForm>
+        }
+      />
     </div>
+  );
+}
+
+const CustomersTable = ({customers, setShowTable}) => {
+  return (
+    <>
+      <Table>
+        <TableHeader>
+          <TableHeaderColumn text="DNI" />
+          <TableHeaderColumn text="Name" />
+          <TableHeaderColumn text="Last Name" />
+          <TableHeaderColumn text="Address" />
+          <TableHeaderColumn text="Sucursal" />
+          <TableHeaderColumn text="Phone" />
+          <TableHeaderColumn text="Email" />
+          <TableHeaderColumn text="Edit" />
+        </TableHeader>
+        <TableBody>
+          {customers.map((customer) => {
+            return <TableRow entity={customer} setShowTable={setShowTable} />;
+          })}
+        </TableBody>
+      </Table>
+    </>
   );
 }
 
